9e4a768b80dad207c92a9b5e668f5980d02213a6
Kamiada <kamiada528@gmail.com>
2019-11-06 13:54:52 +0000

    adding check-in in testing

---
+++ MobileAppForCustomisedTourism/app/src/main/java/com/example/mobileappforcustomisedtourism/CheckIn_inTesting.java
@@ -0,0 +1,14 @@
+package com.example.mobileappforcustomisedtourism;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckIn_inTesting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_in_in_testing);
+    }
+}
